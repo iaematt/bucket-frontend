fcb45cf55bcb1145ad5087c791fea39dd26a8627
Matheus Bastos <matheusbastos@outlook.com>
2019-09-02 08:22:33 -0300

    Fix api URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3001'
+    baseURL: 'https://upload-backend-nodejs.herokuapp.com'
 });
 
 export default api;
